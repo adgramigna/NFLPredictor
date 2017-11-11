461bbac82751f7d142685be33cc02dccf6b13f59
Andrew D.Gramigna <agramign@cs.umass.edu>
2017-11-11 14:40:47 -0500

    Created fixProblems, which manually fixes erros in the .csv files

--- parseplaybyplay.py
+++ parseplaybyplay.py
@@ -11,15 +11,16 @@ def main():
     #pbp2014 = pd.read_csv('pbp-2014.csv') #downloaded from NFLsavant.com/about.php
     pbp2014 = pd.read_csv('pbp-2015.csv')
     #print(pbp2014.loc[12]['GameId'])
-    #pbp2014 = pd.read_csv('pbp-2016.csv')
+    # pbp2014 = pd.read_csv('pbp-2016.csv')
     #sorting to get a reasonable view
     pbp2014 = pbp2014.sort_values(['GameId', 'Quarter', 'Minute', 'Second', 'YardLine'], ascending = [True,True,False,False,False])
     pbp2014 = pbp2014.reset_index(drop=True)
-    #pbp2014.to_csv('pbp2015_test.csv')
+    # pbp2014.to_csv('pbp2015_test.csv')
     gameCount = -1 #initally, how many games are played in a seasons
     game_ids = [] #collecting ids of each unique game
     season_scores = np.zeros((256,2))
     ids_to_teams = {}
+    ids_to_counts = {}
     count=0
     for index in range(pbp2014.shape[0]):
         game_id,game_date,quarter,minute,second,oTeam,dTeam,down,togo,yardline0to100,gotFirstDown,des,season,yardsGained,formation,playType,isRush,\
@@ -27,19 +28,23 @@ def main():
         isTwoPointConversionSuccessful,rushDirection,isPenaltyAccepted,yardlineFixed,yardlineDirection,penTeam,isNoPlay,penType,penYards = getEssentialPlayByPlayFeatures(index,pbp2014)
         #setting up my initial variables of potiential useful features in our play by play data
 
-        must_loop=1
         if quarter == 1 and minute == 15 and second == 0 and playType == 'KICK OFF' and yardline0to100 == 35: #this must occur at the start of every game only once
             if gameCount != -1:
                 prevGameStats = computePrevGameStats(last_game_id,team1,team2,season_scores,team_key,gameCount)
                 print(prevGameStats)
             last_game_id = game_id
             last_game_date = game_date
+            gameCount = gameCount+1
             team1 = oTeam
             team2 = dTeam
-            gameCount = gameCount+1
             game_ids.append(game_id)
             team_key = {team1:0,team2:1}
             ids_to_teams[game_id] = [oTeam,dTeam]
+            ids_to_counts[game_id] = gameCount
+            # if game_id == 2015100500:
+            #     print('gc', gameCount, oTeam,dTeam,season_scores[62][team_key[dTeam]])
+            '''If there are more problems modify above'''
+
             #print(team_key.keys())
             #print(index,game_id, gameCount)
             #getting every unique game in a season
@@ -71,29 +76,20 @@ def main():
         if 'BLOCKED' in des and thisHappened(des,'BLOCKED'): #Blocked kick or punt, find out who recovered the block and adjust teams accordingly
             oTeam,dTeam = blockedRecoverer(des,oTeam,dTeam,down)
 
-        # if isFumble == 1:
-        #     if 'REVERSED' not in des:
-        #         word_counts = Counter(des.split())
-        #     else:
-        #         word_counts= Counter(des.split('REVERSED')[1].split())
-        #     if word_counts.get('FUMBLES') > 1:
-        #         must_loop=word_counts.get('FUMBLES')
-                #pass#print(index,word_counts.get('FUMBLES'), des, isTurnoverFromFumble(isFumble,des,dTeam),thisHappened(des,'FUMBLES'))
-
-        if isTurnoverFromFumble(isFumble,des,oTeam,dTeam) and 'REVERSED' in des and isGoodReversed(des, 'FUMBLES'):
-            pass#print(index,des)
-
         if isFumble ==1:
             potentialFumbleTOs = numTurnoversFromFumble(des) #gets how many times there might have been a turnover from fumble
-            tdes = des
-            for i in range(totalFumbles):
+            tdes = des #So I can check each time with the most recent revocery
+            for i in range(potentialFumbleTOs):
                 if isTurnoverFromFumble(isFumble,tdes,oTeam,dTeam) and thisHappened(des,'FUMBLES'): #if there's a turnover from a fumble, needs to come after INT for edge case where intercepting team fumbles.
-                    print(index,des)           
+                    #print(index,des)           
                     temp = oTeam #swap offense and defense
                     oTeam = dTeam
                     dTeam = temp
                 if potentialFumbleTOs > 1:
-                    tdes = des[des.index('RECOVERED')+1:] #Now checking on the next time a fumble might be recovered
+                    # print(index,tdes,oTeam,dTeam)
+                    if 'RECOVERED' not in tdes:
+                        break #can't be any more potential turnovers from fumbles if no one recovers
+                    tdes = tdes[des.index('RECOVERED')+1:] #Now checking on the next time a fumble might be recovered
 
         if isTouchdown==1 and thisHappened(des,'TOUCHDOWN'): #If touchdown truly occurred, offense team gets 6 points
             season_scores[gameCount][team_key[oTeam]] += 6
@@ -111,10 +107,11 @@ def main():
             season_scores[gameCount][team_key[oTeam]] += 2
             #print(index,oTeam,des)
 
-        
-    fixProblems()
-    print(season_scores)
-    print(ids_to_teams)
+    
+    fixProblems(game_id,season_scores)
+    # print(season_scores)
+    # print(ids_to_teams)
+    # print(ids_to_counts)
     print('xp', count)
 
     checkMissingGames(gameCount, pbp2014, game_ids)
@@ -231,15 +228,18 @@ def computePrevGameStats(game_id,team1,team2,season_scores,team_key,gameCount):
 def isGoodReversed(des, word):
     des = des.replace('.',' ').replace(',',' ').split('REVERSED')
     return word in des[1]
-    # return des.replace('.',' ').replace(',',' ').split().index(word) > des.replace('.',' ').replace(',',' ').split().index('REVERSED')
 
 
 '''The play did indeed occur and the result stood into the next play'''
 def thisHappened(des,word):
     return 'REVERSED' not in des or isGoodReversed(des,word)
 
-def fixProblems():
-    pass
+def fixProblems(game_id,season_scores):
+    problem_count=0
+    problem_count+=1
+    season_scores[62][1] +=1
+    print('Problem: '+str(problem_count)+ ' Fixed. New Score- DET: '+str(int(season_scores[62][1]))+' SEA: '+str(int(season_scores[62][0])))
+            
 
 '''PROBLEMS
 1. M.PRATER one XP not accounted for in table DET vs SEA, score 13-9, should be 13-10. id: 2015100500
